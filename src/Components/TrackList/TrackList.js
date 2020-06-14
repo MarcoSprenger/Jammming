cd37d991f2985ec2c6293b81ae5a51bbac8fb1ba
Marco Sprenger <msprenger99@gmx.ch>
2020-06-15 00:08:39 +0200

    Add tracks to playlist, remove tracks from playlist, pass down searchresult

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./TrackList.css";
-import Track from "../../Components/Track/Track";
+
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <Track />
-        <Track />
-        <Track />
+        {this.props.tracks.map((track) => {
+          return (
+            <Track
+              track={track}
+              key={track.id}
+              onAdd={this.props.onAdd}
+              onRemove={this.props.onRemove}
+              isRemoval={this.props.isRemoval}
+            />
+          );
+        })}
       </div>
     );
   }
